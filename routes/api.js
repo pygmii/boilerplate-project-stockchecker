a085f27f9f677a1352d3e4777ef529cf20942be8
Pichet Suksai <pichet.suksai@7peakssoftware>
2023-12-09 23:17:58 +0700

    init

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      res.json({"stockData":{"stock":"NASDAQ","price":786.90,"likes":1}});
     });
     
 };
